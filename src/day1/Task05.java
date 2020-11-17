7361495af0b7d9784abd00e64a4bb7b5cdb444ec
”MikeStratan” <smi-90@mail.ru>
2020-11-18 02:07:40 +0300

    Day1

---
+++ src/day1/Task05.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task05 {
+    public static void main(String [] args) {
+        int k = 7;
+        for(int counter = 1; counter <10; counter++){
+            System.out.println(counter+" x "+ k +" = "+ counter*k);
+        }
+    }
+}
---
+++ src/ru/stratan/Main.java
@@ -0,0 +1,8 @@
+package ru.stratan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
